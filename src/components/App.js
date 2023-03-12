215c539098c0a14e82624a3f6828b12d8bae4265
kiransahu-dev <61940769+kiransahu-dev@users.noreply.github.com>
2023-03-12 13:33:58 +0530

    updated useref

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,41 @@ import '../styles/App.css';
 const App = () => {
 
 //code here 
+  const inputEmail = useRef(null);
+  const inputPassword = useRef(null);
+  const emailText = useRef(null);
+  const passwordText = useRef(null);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
+  const emailValue = inputEmail.current.value;
+  const passwordValue = inputPassword.current.value;
+  
+  if(emailValue === "" && passwordValue === "")
+  {
+    inputEmail.current.focus();
+  }else if(emailValue === ""){
+    inputEmail.current.focus();
+  }else if(passwordValue === ""){
+    inputPassword.current.focus();
+  }else { 
+    emailText.current.textContent = `Email : ${emailValue}`;
+    passwordText.current.textContent = `Password : ${passwordValue}`;
+  }
+};
 
+  
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={} ref={inputEmail} onChange={handleSubmit}/><br/>
       Password
-      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
+      <input id="inputPassword" type="text" value={} ref={inputPassword} onChange={handleSubmit}/><br/>
+      <button id="submitButton" onClick={handleSubmit}>Submit</button><br/>
+      <p id="emailText">Your Email : {emailValue}</p>
+      <p id ="passwordText">Your Password : {PasswordValue}</p>
       
     </div>
   )
